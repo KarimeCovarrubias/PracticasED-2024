ef81754569d59b5295fce94fad1042f68c85ff17
KarimeCovarrubias <fiftyshades1250@hotmail.com>
2025-03-10 04:22:21 +0000

    ACT PRACTICA1Y2METODONATA //FALTA ACOMODAR LA TABLA EN metodoGaussSeidel

--- metodos/Practica1y2MetodoNata.java
+++ metodos/Practica1y2MetodoNata.java
@@ -1,11 +1,13 @@
 package metodos;
 
+// FALTA ACOMODAR LA TABLA metodoGaussSeidel
+
 import java.util.Scanner;
 import java.text.DecimalFormat;
 
 public class Practica1y2MetodoNata {
     static Scanner leer = new Scanner(System.in);
-    static DecimalFormat formato = new DecimalFormat("#,###.##");
+    static DecimalFormat formato = new DecimalFormat("#,###.######");
     public static void main(String[] args) {
         //
         pantallaInicio();
@@ -21,8 +23,8 @@ public class Practica1y2MetodoNata {
         orden = 3; //leer.nextInt();
         System.out.println();
 
-        String[] concepto = new String[orden];
-        for (int i = 0; i < orden; i++) {
+        String[] concepto = new String[orden + 1];
+        for (int i = 1; i <= orden; i++) {
             System.out.print("CONCEPTO DEL PROBLEMA: ");
             concepto[i] = leer.next();
         }
@@ -30,7 +32,6 @@ public class Practica1y2MetodoNata {
         unidad = leer.next(); 
 
         double[][] matrizA = new double[orden+1][orden + 2];
-        datos(matrizA, orden);
 
         System.out.println("\n-------------------------------------------------------------------------------------------------------------------------------------------\n");
         menu(matrizA, concepto, orden, unidad, pregunta);
@@ -54,7 +55,7 @@ public class Practica1y2MetodoNata {
                     System.out.println("\n-------------------------------------------------------------------------------------------------------------------------------------------\n");
                     
                     System.out.print("PREGUNTA: " + pregunta + "\n");
-                    //metodoGaussSeidel();
+                    metodoGaussSeidel(orden, pregunta, concepto, unidad);
                     break;
                 case 10:
                     System.out.println("¡ADIÓS!");
@@ -78,6 +79,7 @@ public class Practica1y2MetodoNata {
     }
 
     static void metodoGaussJordan(double[][] matrizA, String[] concepto, int orden, String unidad) {
+        datos(matrizA, orden);
         // Impresión de la matriz capturada
         System.out.println("\nMatriz de Datos:");
         imprimirMatriz(matrizA, orden);
@@ -128,17 +130,6 @@ public class Practica1y2MetodoNata {
                 }
             }
         }
-
-        //nata
-        // for (int k = orden; k <= 2; k++) {
-        //     pivote = matrizA[k][k];
-        //     for (int f = 1; f <= k - 1; f++) {
-        //         factor = matrizA[f][k] / pivote;
-        //         for (int c = k; c <= orden + 1; c++) {
-        //             matrizA[f][c] =  matrizA[f][c] - (factor * matrizA[k][c]);
-        //         }
-        //     }
-        // }
         imprimirMatriz(matrizA, orden);
     }
 
@@ -163,12 +154,99 @@ public class Practica1y2MetodoNata {
 
     static void resultados(double[][] matrizA, int orden, String[] concepto, String unidad) {
         for (int i = 1; i <= orden; i++) {
-            System.out.print(concepto[i-1] + " = " + matrizA[i][orden+1] + " " + unidad + ". \n");
+            System.out.print(concepto[i] + " = " + matrizA[i][orden+1] + " " + unidad + ". \n");
         }
     }
 
 // ---------------------------------------------- MÉTODO GAUSS-SEIDEL ----------------------------------------------
-    static void metodoGaussSeidel() {
-        //
+    static void metodoGaussSeidel(int orden, String pregunta, String[] concepto, String unidad) {
+        double[] vant = new double[orden + 1];
+        double[] vact = new double[orden + 1];
+        double[][] matriz = new double[orden + 1][orden + 2];
+
+        for (int f = 1; f <= orden; f++) {
+            System.out.println("Ingrese el valor inicial " + f +": ");
+            vant[f] = leer.nextDouble();
+            vact[f] = 0;
+        }
+        System.out.println("Ingrese el error: ");
+        double error = leer.nextDouble();
+        System.out.println("Ingrese el total de cálculos: 50");
+        int tc = leer.nextInt();
+        System.out.println("------------------------------------------------------------------");
+
+        for (int f = 1; f <= orden; f++) { 
+            for (int c = 1; c <= orden + 1; c++) { 
+                System.out.print("Inserte el dato x" + f + "" + c + ": "); 
+                matriz[f][c] = leer.nextDouble(); 
+            }
+        }
+
+        System.out.println("Impresión del Encabezado incluyendo la pregunta y la información " + 
+                            "tabular de los datos a generar" +
+                            "\nImpresión de los datos iníciales de las incógnitas");
+        System.out.println("\nPREGUNTA: " + pregunta); 
+        System.out.println(" "); 
+        int nc = 0;
+        double errorTotal = 0;
+
+        System.out.println("Número de cálculo = " + nc);
+        for (int f = 1; f <= orden; f++) { 
+            System.out.println(vant[f]);
+        }
+        System.out.println("Error Total = " + errorTotal);
+
+        System.out.println("Calcula el valor de las incógnitas y las guarda en el Vector de " + 
+                            "Valores Actuales para ser comparadas con los Valores anteriores");
+        do { 
+            for (int f = 1; f <= orden; f++) { 
+                double suma = 0; 
+                double coef = matriz[f][f]; 
+                suma = suma + matriz[f][orden + 1]; 
+                for (int c = 1; c <= orden; c++) { 
+                    if (f == c) { 
+                    } else if (c < f) { 
+                        suma = suma + ((matriz[f][c] * (-1.0)) * vact[c]); 
+                    } else { 
+                        suma = suma + ((matriz[f][c] * (-1.0)) * vant[c]); 
+                    } 
+                } 
+                suma = suma / coef; 
+                vact[f] = suma; 
+            }
+
+            System.out.println("\nCálculo del Error Total Absoluto: ");
+            errorTotal = 0; 
+            for (int p = 1; p <= orden; p++) { 
+                errorTotal = errorTotal + Math.abs(Math.abs(vact[p]) - Math.abs(vant[p])); 
+            }
+
+            if (nc == 0) { 
+                for (int i = 1; i <= orden; i++) { 
+                    System.out.print("\t|\t\t" + concepto[i]); 
+                } 
+            }
+
+            System.out.println("\nImpresión de nuevos cálculos: ");
+            System.out.println(""); 
+            nc = nc + 1; 
+            System.out.print(nc + "\t|");
+
+            for (int p = 1; p <= orden; p++) { 
+                System.out.print("\t" + formato.format(vact[p]) + "\t|"); 
+                vant[p] = vact[p]; 
+            }
+            System.out.println("\t" + formato.format(errorTotal) + "\t|"); 
+        } while (errorTotal > error && nc <= tc);
+
+        if (errorTotal <= error) { 
+            System.out.println("\nResultados:"); 
+            for (int p = 1; p <= orden; p++) { 
+                System.out.println(concepto[p] + " = " + vant[p] + " " + unidad);
+                System.out.println(concepto[p] + " = " + Math.round(vant[p]) + " " + unidad);
+            }
+        } else { 
+            System.out.println("El proceso superó el número máximo de cálculos y no encontró la mejor aproximación.");
+        }
     }
 }
